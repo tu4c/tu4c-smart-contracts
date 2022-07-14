2bec3b5827a1a5f5a8e29b48805326807181a092
dbadoy4874 <dbadoy4874@gmail.com>
2022-07-15 03:45:57 +0900

    contracts: add comment in Issuer

--- contracts/Issuer.sol
+++ contracts/Issuer.sol
@@ -4,13 +4,14 @@ pragma solidity ^0.8.12;
 contract Issuer {
     address public dbadoy;
     string public prefix = "https://api.github.com/repos/";
-    string public postfix = "contributors?per_page=1&page=";
+    string public postfix = "contributors?"; // per_page=100&page=
     uint256 public ids = 1;
 
     string[] public prefixHistories;
     string[] public postfixHistories;
 
     mapping (uint256 => string) _urls;
+    mapping (uint256 => string) _enrollSuject;
     mapping (string => uint256) _repoTotalEnrolls;
 
     event Enroll(uint256, string);
@@ -38,17 +39,26 @@ contract Issuer {
 
     // you must remember enroll id. if you forget this, couldn't find it. 
     // in this case, you can loop [1 to latest enroll id] all enrolls id OR enroll again. 
-    function enroll(string memory owner, string memory repo, uint8 pageNum) external returns (uint256) {
-        string memory p = string.concat(prefix, owner, "/", repo, "/", postfix, toString(pageNum));
+    function enroll(string memory subject, string memory owner, string memory repo) external returns (uint256) {
+        string memory p = string.concat(prefix, owner, "/", repo, "/", postfix);
         _urls[ids] = p;
+        _enrollSuject[ids] = subject;
         ids++;
         _repoTotalEnrolls[string.concat(owner, "/", repo)]++;
         emit Enroll(ids - 1, p);
         return ids - 1;
     }
 
-    function getURL(uint256 id) external view returns (string memory) {
-        return _urls[id];
+    // verify flow
+    // 1. getURL from Issuer contracts
+    // 2. get contiributors list (list length: n = undefined)
+    // 3. if _enrollSubject exist that list: true
+    // 4. else: false
+    // -----------------------------------------------------------------------------------
+    // in badge-viewer: service explorer.
+    // in html-converter(or something): provides library that helps verify flow to easily 
+    function getURL(uint256 id) external view returns (string memory, string memory) {
+        return (_urls[id], _enrollSuject[id]);
     }
 
     function getRepoEnrollCount(string memory owner, string memory repo) external view returns (uint256) {
@@ -57,28 +67,6 @@ contract Issuer {
 
     function getUrlTokenPair(uint256 index) external view returns (string memory, string memory) {
         require(prefixHistories.length == postfixHistories.length, "it's may never called");
-        // require(index > prefixHistories.length, "index overflow");
         return (prefixHistories[index], postfixHistories[index]);
     } 
-
-    // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Strings.sol#L15-L35
-    function toString(uint256 value) internal pure returns (string memory) {
-        // Inspired by OraclizeAPI's implementation - MIT licence
-        if (value == 0) {
-            return "0";
-        }
-        uint256 temp = value;
-        uint256 digits;
-        while (temp != 0) {
-            digits++;
-            temp /= 10;
-        }
-        bytes memory buffer = new bytes(digits);
-        while (value != 0) {
-            digits -= 1;
-            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
-            value /= 10;
-        }
-        return string(buffer);
-    }
 }
